4bd8818a636e0454a383b63b3e5af2b567bbe436
Pranav Pratap Singh <pranavpratapsingh108@gmail.com>
2022-04-21 02:31:48 +0530

    created a Simple Tour Model

--- server.js
+++ server.js
@@ -17,6 +17,34 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
+const tourModel = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A Tour must have a name'],
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+});
+
+const Tour = mongoose.model('Tour', tourModel);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  price: 297,
+  rating: 4.2,
+});
+testTour
+  .save()
+  .then((doc) => console.log(`Document saved successfully : ${doc}`))
+  .catch((err) => console.log(`Unabe to save document 💥 ${err}`));
+
 // console.log(process.env.NODE_ENV);
 
 const port = process.env.PORT || 8000;
