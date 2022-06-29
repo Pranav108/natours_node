a9fa9d0f864a734c122411c20bb5200caa089762
Pranav Pratap Singh <pranavpratapsingh108@gmail.com>
2022-06-29 16:33:59 +0530

    Implemented security practices and sending cookies to browser

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -7,28 +12,61 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//  MIDDLEWARE
+//  GLOBAL MIDDLEWARE  //
+//set secuarity HTTP headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
-app.use(express.json());
+
+//Limit request from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests. Please try again after an hour',
+});
+app.use('/api', limiter);
+
+//Body parser,readind data from body to req.body
+app.use(express.json({ limit: '10kb' }));
+
+//data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss());
+
+//Prevent parameter poloution
+app.use(
+  hpp({
+    whitelist: [
+      'ratingsAverage',
+      'ratingsQuantity',
+      'duration',
+      'maxGroupSize',
+      'price',
+    ],
+  })
+);
+
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//Test middleware
 app.use((req, res, next) => {
   req.requestedTime = new Date().toISOString();
-  // console.log(req.headers);
   next();
 });
 
-//ROUTES
+// ROUTES //
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // const err = new Error(`can't find ${req.originalUrl} on this server`);
-  // err.status = 'failure';
-  // err.statusCode = 404;
   next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
 });
 
+// In this middelware all the thrown Error are handeld
 app.use(globalErrorHandler);
 
 //  START SERVER
--- controllers/authController.js
+++ controllers/authController.js
@@ -13,6 +13,15 @@ const signToken = (id) =>
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  user.password = undefined;
+  res.cookie('jwt', token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+  });
+
   res.status(statusCode).json({
     status: 'success',
     token,
