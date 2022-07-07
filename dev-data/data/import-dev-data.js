10aa814081a41c6e60de7a5c5b7eccf24afe828c
Pranav Pratap Singh <pranavpratapsingh108@gmail.com>
2022-07-07 15:22:11 +0530

    implemented basic Modeling and referencing

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const fs = require('fs');
+const path = require('path');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
 
-dotenv.config({ path: './../../config.env' });
+dotenv.config({ path: path.resolve(__dirname, './../../config.env') });
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -15,17 +17,32 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => console.log('DB connection successful'));
 
 //READ JSON FILE
-const tours = JSON.parse(fs.readFileSync(`tours-simple.json`, 'utf-8'));
+const tours = JSON.parse(
+  fs.readFileSync(path.join(__dirname, `tours.json`), 'utf-8')
+);
+const users = JSON.parse(
+  fs.readFileSync(path.join(__dirname, `users.json`), 'utf-8')
+);
 
 //WRITE DATA
 const addToursData = async () => {
   try {
     await Tour.create(tours);
-    console.log('Data successfully loaded');
+    console.log('Tour Data successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+const addUsersData = async () => {
+  try {
+    await User.create(users);
+    console.log('User Data successfully loaded');
   } catch (err) {
     console.log(err);
   }
@@ -35,7 +52,7 @@ const addToursData = async () => {
 //DELETE DATA
 const deleteData = async () => {
   try {
-    await Tour.deleteMany();
+    await User.deleteMany();
     console.log('Data successfully deleted');
   } catch (err) {
     console.log(err);
@@ -43,6 +60,7 @@ const deleteData = async () => {
   process.exit();
 };
 
-if (process.argv[2] === '--import') addToursData();
+if (process.argv[2] === '--importUser') addUsersData();
+else if (process.argv[2] === '--importTour') addToursData();
 else if (process.argv[2] === '--delete') deleteData();
 else console.log(`Can't find what are you looking for`);
---
+++ models/reviewModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review cannot be empty'],
+    },
+    rating: {
+      type: Number,
+      min: [1, 'A rating must be more then or equal to 1'],
+      max: [5, 'A rating must be less then or equal to 5'],
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a Tour'],
+    },
+
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a User'],
+    },
+    createdAt: { type: Date, default: Date.now },
+  },
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
